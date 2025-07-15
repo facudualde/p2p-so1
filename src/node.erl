ad3a3c0f120e50f839ba140a719092512a6505f6
Facundo Dualde <facu.dualde@gmail.com>
2025-07-15 17:12:48 -0300

    Order functions in node file

--- src/node.erl
+++ src/node.erl
@@ -20,42 +20,47 @@
 
 -include_lib("kernel/include/file.hrl").
 
-invalid_name(UdpSocket, Id, Timeout) ->
-  Start = erlang:monotonic_time(millisecond),
-  receive
-    {udp, _Socket, _Ip, _Port, Res} ->
-      Msg = binary_to_list(Res),
-      Tokens = string:tokens(Msg, " \n"),
-      case Tokens of
-        ["INVALID_NAME", _ReqId] ->
-          End = erlang:monotonic_time(millisecond) - Start,
-          Remaining = Timeout - End,
-          invalid_name(UdpSocket, Id, Remaining);
-        _ ->
-          End = erlang:monotonic_time(millisecond) - Start,
-          Remaining = Timeout - End,
-          invalid_name(UdpSocket, Id, Remaining)
-      end
-  after Timeout ->
-    false
-  end.
+run() ->
+  case gen_udp:open(?UDP_PORT,
+                    % Incoming UDP packets are delivered as binaries (<<>>) instead of lists.
+                    [binary,
+                     % The socket is in passive mode, gen_udp:recv/2 must be called to receive data.
+                     {active, true},
+                     % Allows multiple processes to bind to the same port.
+                     {reuseaddr, true},
+                     %         Enables the socket to send or receive broadcast packets.
+                     {broadcast, true},
+                     % Binds the socket to all local network interfaces.
+                     {ip, {0, 0, 0, 0}}])
+  of
+    {ok, UdpSocket} ->
+      io:format("Joining network...~n"),
 
-send_name_request(UdpSocket, Id) ->
-  Msg = list_to_binary("NAME_REQUEST " ++ Id),
-  case gen_udp:send(UdpSocket, {255, 255, 255, 255}, ?UDP_PORT, Msg) of
-    ok ->
-      ok;
+      {Id, InvalidIds} = join_network(UdpSocket, []),
+
+      send_hello(UdpSocket, Id),
+      {HelloPid, HelloRef} = spawn_monitor(?MODULE, hello, [UdpSocket, Id]),
+      register(hello, HelloPid),
+
+      {RemoveInactiveNodesPid, RemoveInactiveNodesRef} =
+        spawn_monitor(?MODULE, remove_inactive_nodes, []),
+      register(remove_inactive_nodes, RemoveInactiveNodesPid),
+
+      TcpSocket = create_tcp_server(),
+      {TcpServerPid, TcpServerRef} = spawn_monitor(?MODULE, run_tcp_server, [TcpSocket, []]),
+      register(tcp_server, TcpServerPid),
+
+      {CliPid, CliRef} = spawn_monitor(?MODULE, cli, [Id]),
+      register(cli, CliPid),
+
+      register(loop, self()),
+      loop(UdpSocket,
+           TcpSocket,
+           Id,
+           InvalidIds,
+           [HelloRef, RemoveInactiveNodesRef, CliRef, TcpServerRef]);
     {error, Reason} ->
-      error({name_request_failed, Reason})
-  end.
-
-get_id(UdpSocket, InvalidIds) ->
-  Id = utils:get_random_id(),
-  case lists:member(Id, InvalidIds) of
-    true ->
-      get_id(UdpSocket, InvalidIds);
-    false ->
-      Id
+      error({udp_open_failed, Reason})
   end.
 
 join_network(UdpSocket, InvalidIds) ->
@@ -77,22 +82,122 @@ join_network(UdpSocket, InvalidIds) ->
       init:stop()
   end.
 
-wait([]) ->
-  ok;
-wait(Refs) ->
-  receive
-    {'DOWN', Ref, process, _Pid, _Reason} ->
-      wait(lists:delete(Ref, Refs))
+send_hello(UdpSocket, Id) ->
+  Msg = list_to_binary("HELLO " ++ Id ++ " " ++ integer_to_list(?TCP_PORT) ++ "\n"),
+  case gen_udp:send(UdpSocket, {255, 255, 255, 255}, ?UDP_PORT, Msg) of
+    ok ->
+      ok;
+    {error, Reason} ->
+      error({send_hello_failed, Reason})
   end.
 
-clean(UdpSocket, TcpSocket, Refs) ->
-  hello ! stop,
-  remove_inactive_nodes ! stop,
-  gen_tcp:close(TcpSocket),
-  wait(Refs),
-  gen_udp:close(UdpSocket),
-  utils:reset_register(),
-  ok.
+hello(UdpSocket, Id) ->
+  Ref = erlang:send_after(15000 + rand:uniform(5000), self(), continue),
+  receive
+    stop ->
+      erlang:cancel_timer(Ref),
+      ok;
+    continue ->
+      send_hello(UdpSocket, Id),
+      hello(UdpSocket, Id)
+  end.
+
+remove_inactive_nodes() ->
+  Ref = erlang:send_after(5000, self(), update),
+  receive
+    stop ->
+      erlang:cancel_timer(Ref),
+      ok;
+    update ->
+      Nodes = utils:load_register(),
+      CurrentTime = erlang:monotonic_time(seconds),
+      Update =
+        maps:filter(fun(_, #{<<"last_seen">> := LastSeen}) -> CurrentTime - LastSeen =< 45 end,
+                    Nodes),
+      utils:save_register(Update),
+      remove_inactive_nodes()
+  end.
+
+create_tcp_server() ->
+  case gen_tcp:listen(?TCP_PORT, [binary, {active, false}, {reuseaddr, true}, {packet, 0}])
+  of
+    {ok, TcpSocket} ->
+      io:format("Tcp server running~n"),
+      TcpSocket;
+    {error, Reason} ->
+      error({tcp_listen_failed, Reason})
+  end.
+
+run_tcp_server(TcpSocket, Refs) ->
+  case gen_tcp:accept(TcpSocket) of
+    {ok, ClientSocket} ->
+      {_, ClientRef} = spawn_monitor(?MODULE, handle_client, [ClientSocket]),
+      run_tcp_server(TcpSocket, [ClientRef | Refs]);
+    {error, closed} ->
+      wait(Refs),
+      ok;
+    {error, Reason} ->
+      error({tcp_server_failed, Reason})
+  end.
+
+cli(Id) ->
+  io:format("~nAvailable commands:~n"),
+  io:format("1: node id~n"),
+  io:format("2: list known nodes in the network~n"),
+  io:format("3: list shared files~n"),
+  io:format("4: list downloaded files~n"),
+  io:format("5: search files in the network~n"),
+  io:format("6: download file from the network~n"),
+  io:format("7: exit~n"),
+  case string:trim(
+         io:get_line("> "))
+  of
+    "1" ->
+      io:format("~nYour id: ~s~n", [Id]),
+      cli(Id);
+    "2" ->
+      utils:show_register(),
+      cli(Id);
+    "3" ->
+      utils:shared_files(),
+      cli(Id);
+    "4" ->
+      utils:downloaded_files(),
+      cli(Id);
+    "5" ->
+      Pattern =
+        string:trim(
+          io:get_line("Pattern: ")),
+      case Pattern of
+        "" ->
+          io:format("~nBad argument~n"),
+          cli(Id);
+        _ ->
+          case distributed_search(Id, Pattern) of
+            empty ->
+              cli(Id);
+            {ref, Ref} ->
+              wait([Ref]),
+              cli(Id)
+          end
+      end;
+    "6" ->
+      FileName =
+        string:trim(
+          io:get_line("File name: ")),
+      NodeId =
+        string:trim(
+          io:get_line("Node id: ")),
+      send_download_request(FileName, NodeId),
+      cli(Id);
+    "7" ->
+      io:format("Terminating program~n"),
+      loop ! stop,
+      ok;
+    _ ->
+      io:format("~nSyntax error, type one of the command numbers above.~n"),
+      cli(Id)
+  end.
 
 loop(UdpSocket, TcpSocket, Id, InvalidIds, Refs) ->
   receive
@@ -133,41 +238,60 @@ loop(UdpSocket, TcpSocket, Id, InvalidIds, Refs) ->
       end
   end.
 
-send_hello(UdpSocket, Id) ->
-  Msg = list_to_binary("HELLO " ++ Id ++ " " ++ integer_to_list(?TCP_PORT) ++ "\n"),
+send_name_request(UdpSocket, Id) ->
+  Msg = list_to_binary("NAME_REQUEST " ++ Id),
   case gen_udp:send(UdpSocket, {255, 255, 255, 255}, ?UDP_PORT, Msg) of
     ok ->
       ok;
     {error, Reason} ->
-      error({send_hello_failed, Reason})
+      error({name_request_failed, Reason})
   end.
 
-hello(UdpSocket, Id) ->
-  Ref = erlang:send_after(15000 + rand:uniform(5000), self(), continue),
+invalid_name(UdpSocket, Id, Timeout) ->
+  Start = erlang:monotonic_time(millisecond),
   receive
-    stop ->
-      erlang:cancel_timer(Ref),
-      ok;
-    continue ->
-      send_hello(UdpSocket, Id),
-      hello(UdpSocket, Id)
+    {udp, _Socket, _Ip, _Port, Res} ->
+      Msg = binary_to_list(Res),
+      Tokens = string:tokens(Msg, " \n"),
+      case Tokens of
+        ["INVALID_NAME", _ReqId] ->
+          End = erlang:monotonic_time(millisecond) - Start,
+          Remaining = Timeout - End,
+          invalid_name(UdpSocket, Id, Remaining);
+        _ ->
+          End = erlang:monotonic_time(millisecond) - Start,
+          Remaining = Timeout - End,
+          invalid_name(UdpSocket, Id, Remaining)
+      end
+  after Timeout ->
+    false
   end.
 
-remove_inactive_nodes() ->
-  Ref = erlang:send_after(5000, self(), update),
-  receive
-    stop ->
-      erlang:cancel_timer(Ref),
-      ok;
-    update ->
-      Nodes = utils:load_register(),
-      CurrentTime = erlang:monotonic_time(seconds),
-      Update =
-        maps:filter(fun(_, #{<<"last_seen">> := LastSeen}) -> CurrentTime - LastSeen =< 45 end,
-                    Nodes),
-      utils:save_register(Update),
-      remove_inactive_nodes()
+get_id(UdpSocket, InvalidIds) ->
+  Id = utils:get_random_id(),
+  case lists:member(Id, InvalidIds) of
+    true ->
+      get_id(UdpSocket, InvalidIds);
+    false ->
+      Id
   end.
+
+wait([]) ->
+  ok;
+wait(Refs) ->
+  receive
+    {'DOWN', Ref, process, _Pid, _Reason} ->
+      wait(lists:delete(Ref, Refs))
+  end.
+
+clean(UdpSocket, TcpSocket, Refs) ->
+  hello ! stop,
+  remove_inactive_nodes ! stop,
+  gen_tcp:close(TcpSocket),
+  wait(Refs),
+  gen_udp:close(UdpSocket),
+  utils:reset_register(),
+  ok.
 
 print(State) ->
   maps:foreach(fun(NodeId, Msgs) ->
@@ -252,65 +376,6 @@ distributed_search(Id, Pattern) ->
       {ref, PrinterRef}
   end.
 
-cli(Id) ->
-  io:format("~nAvailable commands:~n"),
-  io:format("1: node id~n"),
-  io:format("2: list known nodes in the network~n"),
-  io:format("3: list shared files~n"),
-  io:format("4: list downloaded files~n"),
-  io:format("5: search files in the network~n"),
-  io:format("6: download file from the network~n"),
-  io:format("7: exit~n"),
-  case string:trim(
-         io:get_line("> "))
-  of
-    "1" ->
-      io:format("~nYour id: ~s~n", [Id]),
-      cli(Id);
-    "2" ->
-      utils:show_register(),
-      cli(Id);
-    "3" ->
-      utils:shared_files(),
-      cli(Id);
-    "4" ->
-      utils:downloaded_files(),
-      cli(Id);
-    "5" ->
-      Pattern =
-        string:trim(
-          io:get_line("Pattern: ")),
-      case Pattern of
-        "" ->
-          io:format("~nBad argument~n"),
-          cli(Id);
-        _ ->
-          case distributed_search(Id, Pattern) of
-            empty ->
-              cli(Id);
-            {ref, Ref} ->
-              wait([Ref]),
-              cli(Id)
-          end
-      end;
-    "6" ->
-      FileName =
-        string:trim(
-          io:get_line("File name: ")),
-      NodeId =
-        string:trim(
-          io:get_line("Node id: ")),
-      send_download_request(FileName, NodeId),
-      cli(Id);
-    "7" ->
-      io:format("Terminating program~n"),
-      loop ! stop,
-      ok;
-    _ ->
-      io:format("~nSyntax error, type one of the command numbers above.~n"),
-      cli(Id)
-  end.
-
 handle_client(ClientSocket) ->
   case gen_tcp:recv(ClientSocket, 0) of
     {ok, Req} ->
@@ -370,71 +435,6 @@ handle_client(ClientSocket) ->
       end;
     {error, closed} ->
       ok
-  end.
-
-run_tcp_server(TcpSocket, Refs) ->
-  case gen_tcp:accept(TcpSocket) of
-    {ok, ClientSocket} ->
-      {_, ClientRef} = spawn_monitor(?MODULE, handle_client, [ClientSocket]),
-      run_tcp_server(TcpSocket, [ClientRef | Refs]);
-    {error, closed} ->
-      wait(Refs),
-      ok;
-    {error, Reason} ->
-      error({tcp_server_failed, Reason})
-  end.
-
-create_tcp_server() ->
-  case gen_tcp:listen(?TCP_PORT, [binary, {active, false}, {reuseaddr, true}, {packet, 0}])
-  of
-    {ok, TcpSocket} ->
-      io:format("Tcp server running~n"),
-      TcpSocket;
-    {error, Reason} ->
-      error({tcp_listen_failed, Reason})
-  end.
-
-run() ->
-  case gen_udp:open(?UDP_PORT,
-                    % Incoming UDP packets are delivered as binaries (<<>>) instead of lists.
-                    [binary,
-                     % The socket is in passive mode, gen_udp:recv/2 must be called to receive data.
-                     {active, true},
-                     % Allows multiple processes to bind to the same port.
-                     {reuseaddr, true},
-                     %         Enables the socket to send or receive broadcast packets.
-                     {broadcast, true},
-                     % Binds the socket to all local network interfaces.
-                     {ip, {0, 0, 0, 0}}])
-  of
-    {ok, UdpSocket} ->
-      io:format("Joining network...~n"),
-
-      {Id, InvalidIds} = join_network(UdpSocket, []),
-
-      send_hello(UdpSocket, Id),
-      {HelloPid, HelloRef} = spawn_monitor(?MODULE, hello, [UdpSocket, Id]),
-      register(hello, HelloPid),
-
-      {RemoveInactiveNodesPid, RemoveInactiveNodesRef} =
-        spawn_monitor(?MODULE, remove_inactive_nodes, []),
-      register(remove_inactive_nodes, RemoveInactiveNodesPid),
-
-      TcpSocket = create_tcp_server(),
-      {TcpServerPid, TcpServerRef} = spawn_monitor(?MODULE, run_tcp_server, [TcpSocket, []]),
-      register(tcp_server, TcpServerPid),
-
-      {CliPid, CliRef} = spawn_monitor(?MODULE, cli, [Id]),
-      register(cli, CliPid),
-
-      register(loop, self()),
-      loop(UdpSocket,
-           TcpSocket,
-           Id,
-           InvalidIds,
-           [HelloRef, RemoveInactiveNodesRef, CliRef, TcpServerRef]);
-    {error, Reason} ->
-      error({udp_open_failed, Reason})
   end.
 
 send_big_file(FD, ClientSocket, ChunkIndex) ->
